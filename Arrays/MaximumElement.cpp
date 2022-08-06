e10b486d39db4c8a9f7bd3b65507f83fc7fdcc79
KUMARASUBRAHMANYA H <ksbhatfromkana00717@gmail.com>
2022-08-07 00:07:00 +0530

    Create MaximumElement.cpp

---
+++ Arrays/MaximumElement.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int numIndex=0;
+        int count=1;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[numIndex]==nums[i])
+                count++;
+            else
+                count--;
+            if(count==0)
+            {
+                numIndex=i;
+                count=1;
+            }
+        }
+        // return nums[numIndex]; // IF question guarantees max element exists always, then return here. Otherwise below loop is important.
+        count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==nums[numIndex])
+                count++;
+        }
+        if(count>=nums.size()/2)
+            return nums[numIndex];
+    }
+};
