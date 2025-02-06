cbf72410c7bef5796b73866945124cd767072682
Kumarasubrahmanya Hosamane <ksbhatfromkana00717@gmail.com>
2025-02-06 00:34:06 -0800

    Create Jump Game.py

---
+++ Greedy and DP/Jump Game.py
@@ -0,0 +1,15 @@
+# DP is useful when we need all possible paths (e.g., counting ways to reach the end).
+# Here, we only care if reaching the last index is possible, making Greedy a perfect fit.
+
+#The greedy approach dynamically updates the max reachable index as it iterates through the array.
+# It terminates early if it becomes clear that the last index is reachable.
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_reach = 0
+        for i, jump in enumerate(nums):
+            if i>max_reach:
+                return False
+            max_reach = max(max_reach, i+jump)
+            if max_reach >= len(nums)-1:
+                return True
